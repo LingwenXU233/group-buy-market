b1b5d0a578c2d0a90c4f8aa9bb09227f9df40a7f
Lynn XU <xulingwen2000@hotmail.com>
2025-08-08 11:53:10 +0200

    Implement database connection and test DAO layer

---
+++ group-buy-market-lilies-infrastructure/src/main/java/com/liliesrosie/infrastructure/dao/IGroupBuyActivityDao.java
@@ -0,0 +1,14 @@
+package com.liliesrosie.infrastructure.dao;
+
+import com.liliesrosie.infrastructure.dao.po.GroupBuyActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+@Mapper
+public interface IGroupBuyActivityDao {
+
+    List<GroupBuyActivity> queryGroupBuyActivityList();
+
+}
---
+++ group-buy-market-lilies-infrastructure/src/main/java/com/liliesrosie/infrastructure/dao/IGroupBuyDiscountDao.java
@@ -0,0 +1,13 @@
+package com.liliesrosie.infrastructure.dao;
+
+import com.liliesrosie.infrastructure.dao.po.GroupBuyDiscount;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IGroupBuyDiscountDao {
+
+    List<GroupBuyDiscount> queryGroupBuyDiscountList();
+
+}
